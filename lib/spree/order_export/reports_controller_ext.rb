07fa462c843f3abd12a4af745ff0682ede791a72
Ag <github@assembledgoods.com>
2013-01-07 10:36:07 -0600

    Include ship_to and variant options in report.

--- lib/spree/order_export/reports_controller_ext.rb
+++ lib/spree/order_export/reports_controller_ext.rb
@@ -38,10 +38,11 @@ module Spree
                 t('order_export_ext.header.completed_at'),
                 t('order_export_ext.header.number'),
                 t('order_export_ext.header.name'),
-                t('order_export_ext.header.email'),
+                "Ship to",
                 t('order_export_ext.header.variant_name'),
-                t('order_export_ext.header.nonprofit'),
-                t('order_export_ext.header.discount'),
+                "Options",
+                # t('order_export_ext.header.nonprofit'),
+                # t('order_export_ext.header.discount'),
                 t('order_export_ext.header.quantity'),
                 t('order_export_ext.header.order_total'),
                 t('order_export_ext.header.payment_method')
@@ -50,16 +51,23 @@ module Spree
               csv << headers
 
               @orders.each do |order|
+                ship_address = order.ship_address
+                ship_to = "#{ship_address.full_name}"
+                ship_to << " #{ship_address.address1} #{ship_address.address2}"
+                ship_to << " #{ship_address.city} #{ship_address.state_text} #{ship_address.zipcode}"
+                ship_to << " #{ship_address.country.name}"
+                ship_to << " #{ship_address.phone}"
+                
                 order.line_items.each do |line_item|
                   csv_line = []
                   csv_line << order.updated_at
                   csv_line << order.completed_at
                   csv_line << order.number
-                  csv_line << order.name || ""
-                  csv_line << order.email || ""
+                  csv_line << order.name
+                  csv_line << ship_to
                   csv_line << line_item.variant.name
-                  csv_line << line_item.variant.option_values.first(:conditions => "option_type_id = 1").name
-                  csv_line << line_item.variant.option_values.first(:conditions => "option_type_id = 2").name
+                  csv_line << line_item.variant.option_values.map(&:name).join(", ")
+                  # csv_line << line_item.variant.option_values.first(:conditions => "option_type_id = 2").name
                   csv_line << line_item.quantity
                   csv_line << line_item.total.to_s
                   csv_line << order.payment_method.name
